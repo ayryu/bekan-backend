ca3d7ff258d0fb433f2efabf3ec01332a94b2f64
ayryu <ayryu@uwaterloo.ca>
2020-05-21 11:48:01 -0400

    Structure of backend server initialized

---
+++ index.js
@@ -0,0 +1,125 @@
+  const express = require('express')
+  const app = express()
+
+  const cors = require('cors')
+
+  app.use(express.json())
+  app.use(cors())
+
+  let posts = [
+    {
+      id: 0,
+      titleofpost: "Forest0",
+      username: "Matilda",
+      summary: "Dark mode using nature as a backdrop",
+      image: "/static/images/cards/dark-forest.jpg",
+      imagetitle: "Forest with a dark background",
+      verifiedSource: false,
+      date: new Date()
+    },
+
+    {
+      id: 1,
+      titleofpost: "Forest1",
+      username: "Matilda",
+      summary: "Dark mode using nature as a backdrop",
+      image: "/static/images/cards/dark-forest.jpg",
+      imagetitle: "Forest with a dark background",
+      verifiedSource: false,
+      date: new Date()
+    },
+
+    {
+      id: 2,
+      titleofpost: "Forest2",
+      username: "Matilda",
+      summary: "Dark mode using nature as a backdrop",
+      image: "/static/images/cards/dark-forest.jpg",
+      imagetitle: "Forest with a dark background",
+      verifiedSource: false,
+      date: new Date()
+    },
+    {
+      id: 3,
+      titleofpost: "Forest3",
+      username: "Matilda",
+      summary: "Dark mode using nature as a backdrop",
+      image: "/static/images/cards/dark-forest.jpg",
+      imagetitle: "Forest with a dark background",
+      verifiedSource: false,
+      date: new Date()
+    },
+
+    {
+      id: 4,
+      titleofpost: "Forest4",
+      username: "Matilda",
+      summary: "Dark mode using nature as a backdrop",
+      image: "/static/images/cards/dark-forest.jpg",
+      imagetitle: "Forest with a dark background",
+      verifiedSource: false,
+      date: new Date()
+    }, 
+
+    {
+      id: 5,
+      titleofpost: "Forest5",
+      username: "Matilda",
+      summary: "Dark mode using nature as a backdrop",
+      image: "/static/images/cards/dark-forest.jpg",
+      imagetitle: "Forest with a dark background",
+      verifiedSource: false,
+      date: new Date()
+    }
+  ]
+
+  app.get('/', (req, res) => {
+    res.send('<h1>Hello World!</h1>')
+  })
+
+  app.get('/api/posts', (req, res) => {
+    res.json(posts)
+  })
+
+  app.get('/api/posts/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const post = posts.find(post => post.id === id)
+
+    if (post) {
+      response.json(post)
+    } else {
+      response.status(404).end()
+    }
+  })
+
+  app.post('/api/posts', (request, response) => {
+    const postData = request.body
+
+    const post = {
+      titleofpost: postData.title,
+      summary: postData.content,
+      id: postData.id,
+      image: "/static/images/cards/dark-forest.jpg",
+      imagetitle: "Forest with a dark background",
+    }
+
+    posts = posts.concat(post);
+    
+    if (post) {
+      response.json(post)
+    } else {
+      response.status(404).end()
+    }
+    })
+
+  app.delete('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    notes = notes.filter(note => note.id !== id)
+
+    response.status(204).end()
+  })
+
+  const PORT = 3001
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
---
+++ requests/Next Steps.md
@@ -0,0 +1,15 @@
+Next Steps
+
+Front-end:
+Grid of mini-post boxes
+Have a form page
+Look at Material-UI and incorporate some things from there
+Have an About Section
+
+Goal:
+Have cool picture when you first see the homepage
+
+Back-end:
+Be able to handle requests such as:
+GET list of posts
+POST one post
